2203a195131dac71d6d359cc8a54b1a32fd14706
Hugo Taranto <hugotaranto@Hugos-MacBook-Pro.local>
2023-03-15 21:08:51 +1100

    starting second task

---
+++ 2/program.py
@@ -0,0 +1,10 @@
+def task2(filename, letters):
+    #TODO
+    return ''
+
+if __name__ == '__main__':
+    # Example function calls below, you can add your own to test the task2 function
+    print(task2('spain.txt', 'ABE'))
+    print(task2('ai.txt', 'XZ'))
+    print(task2('cabs.txt', 'ABZD'))
+    
