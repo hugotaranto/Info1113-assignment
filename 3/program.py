e258360e26cf4133772d8e82ae2a166441b499b5
Hugo Taranto <hugotaranto@Hugos-MacBook-Pro.local>
2023-03-22 11:46:42 +1100

    setting up task 3

---
+++ 3/program.py
@@ -0,0 +1,10 @@
+def task3(message_filename, dictionary_filename, threshold):
+    #TODO
+    return ''
+
+if __name__ == '__main__':
+    # Example function calls below, you can add your own to test the task3 function
+    print(task3('jingle_bells.txt', 'dict_xmas.txt', 90))
+    print(task3('fruit_ode.txt', 'dict_fruit.txt', 80))
+    print(task3('amazing_poetry.txt', 'common_words.txt', 95))
+    
